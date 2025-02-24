13c0fe95ba0e789ba39a9c488f8d87415eca1343
Durga-Thapa <143153499+Durga-Thapa@users.noreply.github.com>
2025-02-24 15:57:35 +0545

    initial change

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const users= require("./MOCK_DATA.json");
+const app = express();
+const PORT= 8000;
+
+app.get("/api/users", (req, res)=>{
+    return res.json(users);
+});
+
+try {
+    app.get("/api/users/:id", (req, res)=>{
+        const id = req.params.id;
+        const user =users.find((user)=>user.id==id);
+        return res.json({data:user});
+    });
+} catch (err) {
+    return res.json({error:`something went wrong ${err.message}`})
+}
+
+
+app.listen(PORT, ()=> console.log(`server started at port: ${PORT}`));
